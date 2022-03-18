24f52f7c28e5b89e6b621f10a01181d25d262e87
rodrigopluz <rodrigopluz@gmail.com>
2022-03-17 21:20:58 -0300

    :fire: readme correction

---
+++ src/layouts/dashboards/sales/components/DefaultCell/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import MDTypography from "components/MDTypography";
+
+function DefaultCell({ children }: { children: ReactNode }): JSX.Element {
+  return (
+    <MDTypography variant="button" fontWeight="regular" color="text">
+      {children}
+    </MDTypography>
+  );
+}
+
+export default DefaultCell;
